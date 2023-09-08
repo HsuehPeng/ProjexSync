c34b2838b28e8575e89d871d39b1760a98842d4a
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-09-08 19:51:41 +0800

    LoginSceneFactory - To create LoginViewController

--- ProjexSync/ProjexSync/Base/SceneDelegate.swift
+++ ProjexSync/ProjexSync/Base/SceneDelegate.swift
@@ -11,12 +11,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 	var window: UIWindow?
 
-
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let windowScene = (scene as? UIWindowScene) else { return }
 
 		let window = UIWindow(windowScene: windowScene)
-		window.rootViewController = ViewController()
+		let loginSceneFactory = LoginSceneFactory()
+		window.rootViewController = loginSceneFactory.makeLoginScene()
 		self.window = window
 		window.makeKeyAndVisible()
 	}
---
+++ ProjexSync/ProjexSync/Domain/Login/LoginSceneFactory.swift
@@ -0,0 +1,14 @@
+//
+//  LoginSceneFactory.swift
+//  ProjexSync
+//
+//  Created by Hsueh Peng Tseng on 2023/9/8.
+//
+
+import Foundation
+
+class LoginSceneFactory {
+	func makeLoginScene() -> LoginViewController {
+		return LoginViewController()
+	}
+}
