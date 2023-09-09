441cc7347e3eb31d7d19439eb45c3637d9eb976a
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-09-09 17:33:23 +0800

    LoginInteractor.login - present call hide loading indicator when login complete

--- ProjexSync/ProjexSyncTests/Domain/Login/LoginViewControllerInteractorTests.swift
+++ ProjexSync/ProjexSyncTests/Domain/Login/LoginViewControllerInteractorTests.swift
@@ -25,6 +25,16 @@ final class LoginViewControllerInteractorTests: XCTestCase {
 		XCTAssertEqual(presenter.isIndicatorLoading, true)
 	}
 	
+	func test_login_presenterHideShowLoginIndicatorWhenLoginFinish() {
+		let (sut, presenter, loginService) = makeSut()
+		
+		sut.login()
+		
+		loginService.completeLogin(with: nil)
+		
+		XCTAssertEqual(presenter.isIndicatorLoading, false)
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSut() -> (LoginInteractor, LoginViewControllerPresenterMock, LoginServiceMock) {
@@ -57,9 +67,15 @@ final class LoginViewControllerInteractorTests: XCTestCase {
 	
 	private final class LoginServiceMock: LoginService {
 		var loginCallCount = 0
+		var loginCompletions: [LoginCompletion] = []
 		
 		func login(completion: @escaping LoginCompletion) {
 			loginCallCount += 1
+			loginCompletions.append(completion)
+		}
+		
+		func completeLogin(with result: Error?, at index: Int = 0) {
+			loginCompletions[index](result)
 		}
 	}
 }
