2bb8413d6b8f5d6dad0694b23b05d936eb88fd56
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-09-12 20:55:29 +0800

    Extract LoginViewController view to contentView

---
+++ ProjexSync/ProjexSync/Domain/Login/LoginViewControllerView.swift
@@ -0,0 +1,156 @@
+//
+//  LoginViewControllerView.swift
+//  ProjexSync
+//
+//  Created by Hsueh Peng Tseng on 2023/9/12.
+//
+
+import UIKit
+
+final class LoginViewControllerView: ProgrammaticView {	
+	let signinTitleLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Sign In"
+		label.textAlignment = .center
+		label.font = FontConstants.BODY_XL_BOLD
+		label.textColor = ColorConstants.additionalBlack
+		return label
+	}()
+	
+	let emailAddressLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Email Address"
+		label.font = FontConstants.BODY_M_MEDIUM
+		label.textColor = ColorConstants.grayscale70
+		return label
+	}()
+	
+	let emailTextField: BaseTextField = {
+		let textField = BaseTextField(placeHolder: "Enter your email address")
+		textField.translatesAutoresizingMaskIntoConstraints = false
+		return textField
+	}()
+	
+	let passwordLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Password"
+		label.font = FontConstants.BODY_M_MEDIUM
+		label.textColor = ColorConstants.grayscale70
+		return label
+	}()
+	
+	let passwordTextField: HidableTextField = {
+		let textField = HidableTextField(placeHolder: "Enter your password")
+		textField.translatesAutoresizingMaskIntoConstraints = false
+		return textField
+	}()
+	
+	lazy var signInButton: ProjexSyncButton = {
+		let button = ProjexSyncButton(type: .primary, size: .large)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		button.setTitle("Sign In", for: .normal)
+		return button
+	}()
+	
+	let noAccountLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Don’t have an account?"
+		label.font = FontConstants.BODY_L_SEMIBOLD
+		label.textColor = ColorConstants.textGrey
+		return label
+	}()
+	
+	let signUpButton: ProjexSyncButton = {
+		let button = ProjexSyncButton(type: .tertiary, size: .large)
+		button.setTitle("Sign Up", for: .normal)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		return button
+	}()
+	
+	let signUpHsStack: UIStackView = {
+		let view = UIStackView()
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.axis = .horizontal
+		view.spacing = 2
+		return view
+	}()
+	
+	let loadingIndicator: UIActivityIndicatorView = {
+		let view = UIActivityIndicatorView(style: .large)
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.hidesWhenStopped = true
+		view.color = .black
+		return view
+	}()
+	
+	override func configure() {
+		backgroundColor = .white
+	}
+	
+	override func setupConstraint() {
+		addSubview(signinTitleLabel)
+		addSubview(emailAddressLabel)
+		addSubview(emailTextField)
+		addSubview(passwordLabel)
+		addSubview(passwordTextField)
+		addSubview(signInButton)
+		addSubview(signUpHsStack)
+		addSubview(loadingIndicator)
+		
+		NSLayoutConstraint.activate([
+			signinTitleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 70),
+			signinTitleLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+		])
+		
+		NSLayoutConstraint.activate([
+			emailAddressLabel.topAnchor.constraint(equalTo: signinTitleLabel.bottomAnchor, constant: 60),
+			emailAddressLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+			emailAddressLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+		])
+		
+		NSLayoutConstraint.activate([
+			emailTextField.topAnchor.constraint(equalTo: emailAddressLabel.bottomAnchor, constant: 8),
+			emailTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+			emailTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+			emailTextField.heightAnchor.constraint(equalToConstant: 52)
+		])
+		
+		NSLayoutConstraint.activate([
+			passwordLabel.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 16),
+			passwordLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+			passwordLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+		])
+		
+		NSLayoutConstraint.activate([
+			passwordTextField.topAnchor.constraint(equalTo: passwordLabel.bottomAnchor, constant: 8),
+			passwordTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+			passwordTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+			passwordTextField.heightAnchor.constraint(equalToConstant: 52)
+		])
+		
+		NSLayoutConstraint.activate([
+			signInButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 32),
+			signInButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+			signInButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+			signInButton.heightAnchor.constraint(equalToConstant: 56)
+		])
+		
+		signUpHsStack.addArrangedSubview(noAccountLabel)
+		signUpHsStack.addArrangedSubview(signUpButton)
+		NSLayoutConstraint.activate([
+			signUpHsStack.centerXAnchor.constraint(equalTo: centerXAnchor),
+			signUpHsStack.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 32),
+			signUpHsStack.heightAnchor.constraint(equalToConstant: 24)
+		])
+		
+		NSLayoutConstraint.activate([
+			loadingIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
+			loadingIndicator.centerYAnchor.constraint(equalTo: centerYAnchor),
+		])
+	}
+	
+}
--- ProjexSync/ProjexSync/Domain/Login/Scenes/LoginViewController.swift
+++ ProjexSync/ProjexSync/Domain/Login/Scenes/LoginViewController.swift
@@ -16,117 +16,15 @@ protocol LoginViewControllerDisplayLogic: AnyObject {
 class LoginViewController: UIViewController {
 	private let interactor: LoginViewControllerBusinessLogic
 	var router: LoginViewControllerRoutingLogic?
-	
-	// MARK: - UI Elements
-	
-	let signinTitleLabel: UILabel = {
-		let label = UILabel()
-		label.translatesAutoresizingMaskIntoConstraints = false
-		label.text = "Sign In"
-		label.textAlignment = .center
-		label.font = FontConstants.BODY_XL_BOLD
-		label.textColor = ColorConstants.additionalBlack
-		return label
-	}()
-	
-	let emailAddressLabel: UILabel = {
-		let label = UILabel()
-		label.translatesAutoresizingMaskIntoConstraints = false
-		label.text = "Email Address"
-		label.font = FontConstants.BODY_M_MEDIUM
-		label.textColor = ColorConstants.grayscale70
-		return label
-	}()
-	
-	let emailTextField: BaseTextField = {
-		let textField = BaseTextField(placeHolder: "Enter your email address")
-		textField.translatesAutoresizingMaskIntoConstraints = false
-		return textField
-	}()
-	
-	let passwordLabel: UILabel = {
-		let label = UILabel()
-		label.translatesAutoresizingMaskIntoConstraints = false
-		label.text = "Password"
-		label.font = FontConstants.BODY_M_MEDIUM
-		label.textColor = ColorConstants.grayscale70
-		return label
-	}()
-	
-	let passwordTextField: HidableTextField = {
-		let textField = HidableTextField(placeHolder: "Enter your password")
-		textField.translatesAutoresizingMaskIntoConstraints = false
-		return textField
-	}()
-	
-	lazy var signInButton: ProjexSyncButton = {
-		let button = ProjexSyncButton(type: .primary, size: .large)
-		button.translatesAutoresizingMaskIntoConstraints = false
-		button.addTarget(self, action: #selector(didTapSignInButton), for: .touchUpInside)
-		button.setTitle("Sign In", for: .normal)
-		return button
-	}()
-	
-	lazy var firstLineView = makeLineView()
-	
-	let continueWithLabel: UILabel = {
-		let label = UILabel()
-		label.translatesAutoresizingMaskIntoConstraints = false
-		label.text = "Or continue with"
-		label.font = FontConstants.BODY_M_SEMIBOLD
-		label.textColor = ColorConstants.additionalGray
-		return label
-	}()
-	
-	lazy var secondLineView = makeLineView()
-	
-	let continueWithHStack: UIStackView = {
-		let view = UIStackView()
-		view.translatesAutoresizingMaskIntoConstraints = false
-		view.axis = .horizontal
-		view.spacing = 12
-		view.alignment = .center
-		return view
-	}()
-	
-	let noAccountLabel: UILabel = {
-		let label = UILabel()
-		label.translatesAutoresizingMaskIntoConstraints = false
-		label.text = "Don’t have an account?"
-		label.font = FontConstants.BODY_L_SEMIBOLD
-		label.textColor = ColorConstants.textGrey
-		return label
-	}()
-	
-	let signUpButton: ProjexSyncButton = {
-		let button = ProjexSyncButton(type: .tertiary, size: .large)
-		button.setTitle("Sign Up", for: .normal)
-		button.translatesAutoresizingMaskIntoConstraints = false
-		return button
-	}()
-	
-	let signUpHsStack: UIStackView = {
-		let view = UIStackView()
-		view.translatesAutoresizingMaskIntoConstraints = false
-		view.axis = .horizontal
-		view.spacing = 2
-		return view
-	}()
-	
-	let loadingIndicator: UIActivityIndicatorView = {
-		let view = UIActivityIndicatorView(style: .large)
-		view.translatesAutoresizingMaskIntoConstraints = false
-		view.hidesWhenStopped = true
-		view.color = .black
-		return view
-	}()
+	lazy var contentView = LoginViewControllerView()
 	
 	// MARK: - LifeCycle
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = .white
-		configureLayout()
+		
+		view = contentView
+		contentView.signInButton.addTarget(self, action: #selector(didTapSignInButton), for: .touchUpInside)
 	}
 	
 	init(interactor: LoginViewControllerBusinessLogic) {
@@ -139,7 +37,7 @@ class LoginViewController: UIViewController {
 	}
 	
 	@objc func didTapSignInButton() {
-		interactor.loginWith(email: emailTextField.text, password: passwordTextField.text)
+		interactor.loginWith(email: contentView.emailTextField.text, password: contentView.passwordTextField.text)
 	}
 }
 
@@ -153,101 +51,6 @@ extension LoginViewController: LoginViewControllerDisplayLogic {
 	}
 	
 	func loginLoadingIndicator(shouldShow: Bool) {
-		shouldShow ? loadingIndicator.startAnimating() : loadingIndicator.stopAnimating()
+		shouldShow ? contentView.loadingIndicator.startAnimating() : contentView.loadingIndicator.stopAnimating()
 	}
 }
-
-// MARK: - Layout
-
-extension LoginViewController {
-	private func configureLayout() {
-		view.addSubview(signinTitleLabel)
-		view.addSubview(emailAddressLabel)
-		view.addSubview(emailTextField)
-		view.addSubview(passwordLabel)
-		view.addSubview(passwordTextField)
-		view.addSubview(signInButton)
-		view.addSubview(continueWithHStack)
-		view.addSubview(signUpHsStack)
-		view.addSubview(loadingIndicator)
-		
-		NSLayoutConstraint.activate([
-			signinTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 70),
-			signinTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-		])
-		
-		NSLayoutConstraint.activate([
-			emailAddressLabel.topAnchor.constraint(equalTo: signinTitleLabel.bottomAnchor, constant: 60),
-			emailAddressLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-			emailAddressLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
-		])
-		
-		NSLayoutConstraint.activate([
-			emailTextField.topAnchor.constraint(equalTo: emailAddressLabel.bottomAnchor, constant: 8),
-			emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-			emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
-			emailTextField.heightAnchor.constraint(equalToConstant: 52)
-		])
-		
-		NSLayoutConstraint.activate([
-			passwordLabel.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 16),
-			passwordLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-			passwordLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
-		])
-		
-		NSLayoutConstraint.activate([
-			passwordTextField.topAnchor.constraint(equalTo: passwordLabel.bottomAnchor, constant: 8),
-			passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-			passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
-			passwordTextField.heightAnchor.constraint(equalToConstant: 52)
-		])
-		
-		NSLayoutConstraint.activate([
-			signInButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 32),
-			signInButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
-			signInButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
-			signInButton.heightAnchor.constraint(equalToConstant: 56)
-		])
-		
-		NSLayoutConstraint.activate([
-			firstLineView.widthAnchor.constraint(equalToConstant: 62),
-			firstLineView.heightAnchor.constraint(equalToConstant: 1)
-		])
-		
-		NSLayoutConstraint.activate([
-			secondLineView.widthAnchor.constraint(equalToConstant: 62),
-			secondLineView.heightAnchor.constraint(equalToConstant: 1)
-		])
-		
-		continueWithHStack.addArrangedSubview(firstLineView)
-		continueWithHStack.addArrangedSubview(continueWithLabel)
-		continueWithHStack.addArrangedSubview(secondLineView)
-		NSLayoutConstraint.activate([
-			continueWithHStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			continueWithHStack.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 32),
-			continueWithHStack.heightAnchor.constraint(equalToConstant: 22)
-		])
-		
-		signUpHsStack.addArrangedSubview(noAccountLabel)
-		signUpHsStack.addArrangedSubview(signUpButton)
-		NSLayoutConstraint.activate([
-			signUpHsStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			signUpHsStack.topAnchor.constraint(equalTo: continueWithHStack.bottomAnchor, constant: 32),
-			signUpHsStack.heightAnchor.constraint(equalToConstant: 24)
-		])
-		
-		NSLayoutConstraint.activate([
-			loadingIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			loadingIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-		])
-		
-	}
-	
-	private func makeLineView() -> UIView {
-		let view = UIView()
-		view.translatesAutoresizingMaskIntoConstraints = false
-		view.backgroundColor = ColorConstants.additionalGray
-		return view
-	}
-}
-
