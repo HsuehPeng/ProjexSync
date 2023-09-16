47ea5e63070cff39ee15f94f7456ea0b0f85d5fd
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-09-16 22:26:56 +0800

    EmailPasswordValidator - Add Email&Password validation function

--- ProjexSync/ProjexSyncTests/FrameWorks/Login/EmailPasswordValidatorTests.swift
+++ ProjexSync/ProjexSyncTests/FrameWorks/Login/EmailPasswordValidatorTests.swift
@@ -8,8 +8,14 @@
 import XCTest
 
 class EmailPasswordValidator {
-	func validateEmail(_ email: String) -> Bool {
-		return false
+	func isValidEmail(_ email: String) -> Bool {
+		let emailValidationRegex = "^[\\p{L}0-9!#$%&'*+\\/=?^_`{|}~-][\\p{L}0-9.!#$%&'*+\\/=?^_`{|}~-]{0,63}@[\\p{L}0-9-]+(?:\\.[\\p{L}0-9-]{2,7})*$"
+		let emailValidationPredicate = NSPredicate(format: "SELF MATCHES %@", emailValidationRegex)
+		return emailValidationPredicate.evaluate(with: email)
+	}
+	
+	func isValidPassword(_ password: String) -> Bool {
+		return password.count >= 6
 	}
 }
 
@@ -18,8 +24,36 @@ final class EmailPasswordValidatorTests: XCTestCase {
 		let sut = EmailPasswordValidator()
 		let wrongEmail = "WrongEmail"
 		
-		let isValidEmail = sut.validateEmail(wrongEmail)
+		let isValidEmail = sut.isValidEmail(wrongEmail)
 		
 		XCTAssertEqual(isValidEmail, false)
 	}
+	
+	func test_validateEmail_returnTrueWhenEmailIsCorrect() {
+		let sut = EmailPasswordValidator()
+		let correctEmail = "123@gmail.com"
+		
+		let isValidEmail = sut.isValidEmail(correctEmail)
+		
+		XCTAssertEqual(isValidEmail, true)
+	}
+	
+	func test_validatePassword_returnFalseWhenPasswordFormateIsInValid() {
+		let sut = EmailPasswordValidator()
+		let wrongPassword = "00000"
+		
+		let isValidPassword = sut.isValidPassword(wrongPassword)
+		
+		XCTAssertEqual(isValidPassword, false)
+	}
+	
+	func test_validatePassword_returnTrueWhenPasswordFormateIsInValid() {
+		let sut = EmailPasswordValidator()
+		let correctPassword = "000000"
+		
+		let isValidPassword = sut.isValidPassword(correctPassword)
+		
+		XCTAssertEqual(isValidPassword, true)
+	}
+	
 }
