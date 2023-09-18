26c7901d32546b8a1c8823d421d600e829fd3c8d
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-09-18 20:59:33 +0800

    ProjectListViewControllerFactory creation

---
+++ ProjexSync/ProjexSync/Domain/ProjectList/ProjectListViewControllerFactory.swift
@@ -0,0 +1,14 @@
+//
+//  ProjectListViewControllerFactory.swift
+//  ProjexSync
+//
+//  Created by Hsueh Peng Tseng on 2023/9/18.
+//
+
+import UIKit
+
+final class ProjectListViewControllerFactory {
+	static func makeProjectListScene() -> ProjectListViewController {
+		return ProjectListViewController()
+	}
+}
