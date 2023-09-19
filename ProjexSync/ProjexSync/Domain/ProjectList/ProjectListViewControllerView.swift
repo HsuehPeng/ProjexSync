055a64d982c5c5297cd20ef5eb83c8a19321cb2e
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-09-19 17:36:06 +0800

    ProjectListViewControllerView - Add tableView

--- ProjexSync/ProjexSync/Domain/ProjectList/ProjectListViewControllerView.swift
+++ ProjexSync/ProjexSync/Domain/ProjectList/ProjectListViewControllerView.swift
@@ -15,6 +15,12 @@ final class ProjectListViewControllerView: ProgrammaticView {
 		return textField
 	}()
 	
+	lazy var tableView: UITableView = {
+		let tableView = UITableView()
+		tableView.translatesAutoresizingMaskIntoConstraints = false
+		return tableView
+	}()
+	
 	override func configure() {
 		backgroundColor = .white
 		
@@ -23,12 +29,20 @@ final class ProjectListViewControllerView: ProgrammaticView {
 	
 	override func setupConstraint() {
 		addSubview(searchTextField)
+		addSubview(tableView)
 		
 		NSLayoutConstraint.activate([
 			searchTextField.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 28),
 			searchTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
 			searchTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
 			searchTextField.heightAnchor.constraint(equalToConstant: 52)
+		])
+		
+		NSLayoutConstraint.activate([
+			tableView.topAnchor.constraint(equalTo: searchTextField.bottomAnchor, constant: 24),
+			tableView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+			tableView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+			tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
 		])
 		
 	}
