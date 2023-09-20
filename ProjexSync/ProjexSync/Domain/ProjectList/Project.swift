9cc636184d9d187041ed6e69079ce8cdc1af9f1d
Hsueh Peng Tseng <qaz5899971@gmail.com>
2023-09-20 20:21:55 +0800

    ProjectListLoadingWorker - Load project list prelimary function

--- ProjexSync/ProjexSync/Domain/ProjectList/Project.swift
+++ ProjexSync/ProjexSync/Domain/ProjectList/Project.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-struct Project {
-	
+struct Project: Codable {
+	let name: String
 }
--- ProjexSync/ProjexSync/Domain/ProjectList/Scene/ProjectListLoadingWorker.swift
+++ ProjexSync/ProjexSync/Domain/ProjectList/Scene/ProjectListLoadingWorker.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+import FirebaseFirestoreSwift
 
 protocol ProjectListLoadingLogic {
 	typealias LoadResult = Result<[Project], Error>
@@ -14,6 +17,21 @@ protocol ProjectListLoadingLogic {
 
 final class ProjectListLoadingWorker: ProjectListLoadingLogic {
 	func load(completion: @escaping (LoadResult) -> Void) {
-		
+		let ref = Firestore.firestore().collection("User").document("OFohDxvHlahAOB4YVQc6AKuyG4e2").collection("Project")
+		ref.getDocuments { snapshot, error in
+			if let error = error {
+				completion(.failure(error))
+			} else {
+				var projects = [Project]()
+				for document in snapshot!.documents {
+					let data = document.data()
+					
+					let project = Project(name: data["name"] as! String)
+					projects.append(project)
+				}
+				
+				completion(.success(projects))
+			}
+		}
 	}
 }
